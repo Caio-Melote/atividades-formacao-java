bd09f2bad352099e9a9ef6661ba0487a896c2f76
Caio Melo Teixeira <tcx3@outlook.com>
2024-09-20 18:01:25 -0300

    Melhoria na tratativa de exceçoes
    Ajuste no Try/Catch para exceçao na entrada

--- ConsultaGitHub/src/br/com/consultagithub/main/Main.java
+++ ConsultaGitHub/src/br/com/consultagithub/main/Main.java
@@ -1,67 +1,62 @@
 package br.com.consultagithub.main;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
-//import javax.print.attribute.SetOfIntegerSyntax;
-
 import com.google.gson.Gson;
-
 import br.com.consultagithub.modelo.UserModel;
 import br.com.consultagithub.record.RecordJson;
 
 public class Main {
-	public static void main(String[] args) throws IOException, InterruptedException {
+	public static void main(String[] args) {
 		String nomeUsuario = "";
-		Scanner captura = new Scanner(System.in); 
+		Scanner captura = new Scanner(System.in);
 		List<UserModel> listaUsuarios = new ArrayList<UserModel>();
-		
-		while(!nomeUsuario.equals("sair")) {
+
+		while (!nomeUsuario.equals("sair")) {
 			System.out.println("Digite o username do usuário que deseja buscar: ");
-			nomeUsuario = captura.next();
-			
+			nomeUsuario = captura.nextLine();
+
 			if (nomeUsuario.equals("sair")) {
 				break;
 			}
-			
-			//Cliente (HTTP)
+
+			// Cliente (HTTP)
 			HttpClient client = HttpClient.newHttpClient();
-			
-			//Request (HTTP)
-			HttpRequest request = HttpRequest.newBuilder()
-				      .uri(URI.create("https://api.github.com/users/" + nomeUsuario))
-				      .GET()
-				      .build();
+
 			try {
-				HttpResponse <String> resposta = client.send(request, HttpResponse.BodyHandlers.ofString());	
-				
-				if(resposta.statusCode() == 404) {
-					throw new Exception("Usuário não encontrado: " + nomeUsuario);
-				}else {
-				Gson gson = new Gson();
-				
-				RecordJson usuario = gson.fromJson(resposta.body(),RecordJson.class);
-				UserModel novoUsuario = new UserModel(usuario);
-				listaUsuarios.add(novoUsuario);	
+				// Request (HTTP)
+				HttpRequest request = HttpRequest.newBuilder()
+						.uri(URI.create("https://api.github.com/users/" + nomeUsuario)).GET().build();
+
+				HttpResponse<String> resposta = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+				if (resposta.statusCode() == 404) {
+					System.out.println("!!Username INVÁLIDO!!");
+				} else {
+					Gson gson = new Gson();
+
+					RecordJson usuario = gson.fromJson(resposta.body(), RecordJson.class);
+					UserModel novoUsuario = new UserModel(usuario);
+					listaUsuarios.add(novoUsuario);
 				}
-			} catch (Exception e) {
+			} catch (IllegalArgumentException e) {
+                System.out.println("Erro na URI: " + e.getMessage());
+            }catch (Exception e) {
 				System.out.println("Erro!!\n" + e + "\n");
 			}
-	
+
 		}
-		
-		for(UserModel user : listaUsuarios) {
+
+		for (UserModel user : listaUsuarios) {
 			System.out.println(user);
 		}
-		
-		//System.out.println("Usuário println: " + listaUsuarios.toString());
+
+		// System.out.println("Usuário println: " + listaUsuarios.toString());
 		captura.close();
 	}
 }
