b0ae613e4379aca72fb6595ebff01238b54203be
Caio Melo Teixeira <tcx3@outlook.com>
2024-09-30 17:53:07 -0300

    Avanço JPA
    Melhoria no código com adição de diversas funcionalidades incluindo vários tipos de consultas.

--- Estabelecimento/src/main/java/br/com/jpa/estabelecimento/dao/ProdutoDao.java
+++ Estabelecimento/src/main/java/br/com/jpa/estabelecimento/dao/ProdutoDao.java
@@ -2,6 +2,8 @@ package br.com.jpa.estabelecimento.dao;
 
 import javax.persistence.EntityManager;
 
+import java.math.BigDecimal;
+import java.util.List;
 import br.com.jpa.estabelecimento.modelo.Produto;
 
 public class ProdutoDao {
@@ -20,5 +22,42 @@ public class ProdutoDao {
 		this.em.merge(produto);
 	}
 	
+	public Produto buscarId(Long id) {
+		return em.find(Produto.class, id);
+	}
+	
+	public List<Produto> buscarTodos(){
+		String jpql = "SELECT p FROM Produto p";
+		return em.createQuery(jpql, Produto.class).getResultList();
+	}
+	
+	public List<Produto> buscarNome(String nome){
+		String jpql = "SELECT p FROM Produto p WHERE p.nome = ?1";
+		return em.createQuery(jpql, Produto.class)
+				.setParameter(1, nome)
+				.getResultList();
+	}
+	
+/*	public List<Produto> buscarNome(String nome){
+		String jpql = "SELECT p FROM Produto p WHERE p.nome = :naime";
+		return em.createQuery(jpql, Produto.class)
+				.setParameter("naime", nome)
+				.getResultList();
+	}*/
+	
+	
+	public List<Produto> buscarCategoria(String nome){
+		String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = ?1";
+		return em.createQuery(jpql, Produto.class)
+				.setParameter(1, nome)
+				.getResultList();
+	}
+	
+	public BigDecimal buscarPrecoComNome(String nome){
+		String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = ?1";
+		return em.createQuery(jpql, BigDecimal.class)
+				.setParameter(1, nome)
+				.getSingleResult(); //Método para carregar um único registro
+	}
 	
 }
--- Estabelecimento/src/main/java/br/com/jpa/estabelecimento/modelo/CadastroProduto.java
+++ Estabelecimento/src/main/java/br/com/jpa/estabelecimento/modelo/CadastroProduto.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import java.util.List;
 import br.com.jpa.estabelecimento.dao.CategoriaDao;
 import br.com.jpa.estabelecimento.dao.ProdutoDao;
 import br.com.jpa.estabelecimento.util.JpaUtil;
@@ -13,9 +14,60 @@ import br.com.jpa.estabelecimento.util.JpaUtil;
 public class CadastroProduto {
 	public static void main(String[] args) {
 		
-		Categoria novaCategoria = new Categoria("TV");
+		//cadastrarProduto();
 		
-		Produto novoProduto = new Produto("Samsung 60''","120Hz", new BigDecimal("3400.99"), novaCategoria);
+		//Busca por ID
+		EntityManager em = JpaUtil.getEntityManager();
+		ProdutoDao daoProduto = new ProdutoDao(em);
+		Produto p = daoProduto.buscarId(2l);
+		System.out.println("Imprimindo preço do produto buscado por ID: ");
+		System.out.println(p.getPreco());
+		System.out.println("=-------------------------------------= ");
+		
+		//Busca todos e coloca em uma lista
+		List<Produto> listaProdutos = daoProduto.buscarTodos();
+		
+		//Busca filtrando e coloca em uma lista
+		List<Produto> listaFiltrados = daoProduto.buscarNome("BRASTEMP");
+		
+		//Busca filtrando categoria e coloca em uma lista
+		List<Produto> listaCategoriaList = daoProduto.buscarCategoria("GELADEIRA");
+		
+		//Busca apenas 1 registro
+		BigDecimal precoProduto = daoProduto.buscarPrecoComNome("BRASTEMP");
+		System.out.println("=------------------------=");
+		System.out.println("O preco do produto 'BRASTEMP' é: "+ precoProduto);
+		
+		//Primeira maneira de imprimir toda a lista
+		//listaProdutos.forEach(p2 -> System.out.println(p.getDescricao())); 
+		
+		//Segunda maneira de imprimir toda a lista
+			//Lista de todos
+		System.out.println("=-------------------------------------= ");
+		System.out.println("Imprimindo lista de nomes de todos os produtos: ");
+		for(Produto prod : listaProdutos) {
+			System.out.println(prod.getNome());
+		}
+		
+			//Lista de produtos filtrados
+		System.out.println("=-------------------------------------= ");
+		System.out.println("Imprimindo lista de nomes dos produtos filtrados: ");		
+		for(Produto prd : listaFiltrados) {
+			System.out.println(prd.getNome());
+		}
+		
+			//Lista filtrada por categoria
+		System.out.println("=-------------------------------------= ");
+		System.out.println("Imprimindo lista nomes filtrados por categoria: ");		
+		for(Produto cat : listaCategoriaList) {
+			System.out.println(cat.getNome());
+		}
+	}
+
+	private static void cadastrarProduto() {
+		
+		Categoria novaCategoria = new Categoria("GELADEIRA");		
+		Produto novoProduto = new Produto("BRASTEMP","487L Frost Free", new BigDecimal("5690.50"), novaCategoria);
 		
 		EntityManager em = JpaUtil.getEntityManager();
 		
